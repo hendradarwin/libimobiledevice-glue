fbb859aff1580f452e8d16c10babe16e82de7c94
Hendra Darwintha <hendra@accelbyte.net>
2025-03-27 05:43:35 +0700

    Temporary fix to make it's working properly on the macos platform

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,9 @@
 from conan import ConanFile
-from conan.tools.layout import basic_layout
+from conan.tools.cmake import cmake_layout
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 from conan.tools.files import copy
 from os.path import join
+from conan.tools.apple import fix_apple_shared_install_name
 
 class libplistConan(ConanFile):
     name = "libimobiledevice-glue"
@@ -37,7 +38,7 @@ class libplistConan(ConanFile):
             
 
     def layout(self):
-        basic_layout(self)
+        cmake_layout(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -51,10 +52,35 @@ class libplistConan(ConanFile):
         cmake.build()
 
     def package(self):
-        # Make sure the Macros.cmake is packaged
-        # copy(self, "*.cmake", src=self.source_folder, dst=self.package_folder)
         cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["imobiledevice-glue"]
+        # self.cpp_info.libs = ["imobiledevice-glue"]
+        platform_path = "windows"
+        if self.settings.os=="Linux":
+            platform_path = "linux"
+        elif self.settings.os=="Macos":    
+            platform_path = "macos"
+        
+        configuration_path = ""
+        if self.settings.build_type == "Debug":
+            configuration_path = "debug"
+        
+        custom_relative_path = join("x64", platform_path, configuration_path)
+
+        # lib_path = join("lib", custom_relative_path)
+        # bin_path = join("bin", custom_relative_path)        
+        lib_path = "lib"
+        bin_path = "bin"
+
+        self.cpp_info.set_property("cmake_file_name", "libimobiledevice-glue")
+        self.cpp_info.set_property("cmake_target_name", "libimobiledevice-glue::libimobiledevice-glue")
+        self.cpp_info.set_property("cmake_find_mode", "both")
+        self.cpp_info.set_property("pkg_config_name", "libimobiledevice-glue")        
+        
+        self.cpp_info.libdirs = [lib_path]   
+        self.cpp_info.libs = ["libimobiledevice-glue"]
+        self.cpp_info.bindirs = [bin_path]
+        self.cpp_info.components["libimobiledevice-glue"].libs = ["libimobiledevice-glue.a"]
+        fix_apple_shared_install_name(self)        
